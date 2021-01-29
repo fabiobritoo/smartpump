862c97b21c6e68c758ce2f6117bb117ffbd6f8df
Fabio Brito <araujo.fabio7@gmail.com>
2021-01-29 17:48:34 -0300

    Flow Sensor OK

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -16,7 +16,6 @@ const char* password = "12345678";
 #define FLOWPIN 16                                // Pino D0
 #define RELAYPIN 14                               // Pino D5 (Recebimento de Mensagens)
  
-float flow = 0;                          // Variável que armazena o valor da flow
 int wifiStatus;
 
 int timezone = -3 * 3600;                     // Definição da TimeZone
@@ -64,6 +63,26 @@ PubSubClient client(host, 1883, NULL, wifiClient);
 
 DynamicJsonDocument doc(1024);                    // Variável para Converter o Json recebido
 
+
+
+// Variáveis do sensor de Vazão
+void ICACHE_RAM_ATTR handleInterrupt();
+
+#define PULSE_PIN D2  //gpio4
+#define FLOW_CALIBRATION 8.2 
+
+volatile long pulseCount = 0;
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+float totalLitres;
+float totalLitresold;
+unsigned long oldTime;
+
+///////
+
+
+///////////////////////////////////////////////////
 
 // Método que atua no valor recebido pelo tópico de entrada
 void checkPump(int val){
@@ -134,7 +153,68 @@ String preparePayload (float data){
   return payload;
 
 }
+//__ Envia os dados para a cloud
 
+
+void enviaDado(float dado){
+ 
+  String payload = preparePayload(dado);
+  Serial.println(payload);
+
+ //__ Envia o dado
+ 
+  if (client.publish(topic, (char*) payload.c_str())) {
+    Serial.println("Publish ok");
+  } else {
+    Serial.println("Publish failed");
+  }
+ 
+}
+
+/// Sensor de Vazão
+
+void ICACHE_RAM_ATTR pulseCounter()
+{
+  pulseCount++;
+}
+
+void flow()
+{
+
+  if ((millis() - oldTime) > 1000)   // Only process counters once per second
+  {
+    detachInterrupt(PULSE_PIN);
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / FLOW_CALIBRATION;
+    oldTime = millis();
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
+    totalLitres = totalLitresold + totalMilliLitres * 0.001;
+    unsigned int frac;
+
+    // Print the flow rate for this second in liters / minute
+    Serial.print("flowrate: ");
+    Serial.print(int(flowRate));  // Print the integer part of the variable
+
+    Serial.print(".");             // Print the decimal point
+    frac = (flowRate - int(flowRate)) * 10; // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
+    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+    Serial.print("L/min");
+
+    // Serial.print("  Current Liquid Flowing: ");  // Print the number of liters flowed in this second
+    // Serial.print(flowMilliLitres);
+    // Serial.print("mL/Sec");
+
+    // Serial.print("  Output Liquid Quantity: ");  // Print the cumulative total of liters flowed since starting
+    // Serial.print(totalLitres);
+    // Serial.println("L");
+
+    pulseCount = 0;  // Reset the pulse counter so we can start incrementing again
+
+    attachInterrupt(PULSE_PIN, pulseCounter, FALLING);    // Enable the interrupt again now that we've finished sending output
+    enviaDado(flowRate);
+  }
+
+}
 
 void reconnect() {
   // Loop until we're reconnected
@@ -194,40 +274,40 @@ void setup() {
   // Define função callback
   client.setCallback(callback);
 
+  // Sensor de Vazão
+
+    pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+  totalLitresold = 0;
+
+  pinMode(PULSE_PIN, INPUT);  // Initialization of the variable "PULSE_PIN" as INPUT (D2 pin)
+
+  attachInterrupt(PULSE_PIN, pulseCounter, FALLING);
+
+
   // Allow the hardware to sort itself out
   delay(1500);
 
   
 }
 
-//__ Envia os dados para a cloud
 
-
-void enviaDado(float dado){
- 
-  String payload = preparePayload(dado);
-  Serial.println(payload);
-
- //__ Envia o dado
- 
-  if (client.publish(topic, (char*) payload.c_str())) {
-    Serial.println("Publish ok");
-  } else {
-    Serial.println("Publish failed");
-  }
- 
-}
 
  
 void loop() {
 
-  unsigned long currentMillis = millis();
+  //unsigned long currentMillis = millis();
   if (!client.connected()) {
     checkPump(0);
     reconnect();
   }
 
   client.loop(); 
+
+  flow();
 
   //__ Le Sensores
   //flow = LeTemperatura();
